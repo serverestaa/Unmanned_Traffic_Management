6fb35f9d3a1d21b435a33bb9425dafd48c5ed668
Dastan Tynyshtyk <dasiktazik@gmail.com>
2025-05-25 02:11:24 +0500

    font fix last

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { GeistSans } from 'geist/font/sans';
-import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 import "./globals.css";
 import { Providers } from "@/providers";
 import { Header } from "@/widgets/Header";
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable} `}>
       <body
         className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
       >
@@ -27,7 +27,6 @@ export default function RootLayout({
             <Header />
             {children}
           </main>
-          
         </Providers>
       </body>
     </html>
